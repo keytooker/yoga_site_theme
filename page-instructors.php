2cd2459d824590d5f3cf14169043ffbc97225bae
Mikhail Visloguzov <keytooker@gmail.com>
2020-01-25 11:02:51 +0300

    Страница инструкторов; страница блога.

--- page-instructors.php
+++ page-instructors.php
@@ -39,26 +39,26 @@
 			<div>
 				<ul class="section">
 					<li>
-						<img src="images/instructor1.jpg" alt="Yoga instructor one">
+						<img src="<?php bloginfo('template_url' ); ?>/images/instructor1.jpg" alt="Yoga instructor one">
 						<h4>Belle Humpfree</h4>
 					</li>
 					<li>
-						<img src="images/instructor2.jpg" alt="Yoga instructor two">
+						<img src="<?php bloginfo('template_url' ); ?>/images/instructor2.jpg" alt="Yoga instructor two">
 						<h4>Jacob Collern</h4>
 					</li>
 				</ul>
 				<ul class="section">
 					<li>
-						<img src="images/instructor3.jpg" alt="Yoga instructor three">
+						<img src="<?php bloginfo('template_url' ); ?>/images/instructor3.jpg" alt="Yoga instructor three">
 						<h4>Edward Blanche</h4>
 					</li>
 					<li>
-						<img src="images/instructor4.jpg" alt="Yoga instructor four">
+						<img src="<?php bloginfo('template_url' ); ?>/images/instructor4.jpg" alt="Yoga instructor four">
 						<h4>Carrie Waldorf</h4>
 					</li>
 				</ul>
 			</div>
-			<img src="images/lady-in-yoga.jpg" alt="lady doing yoga" class="figure">
+			<img src="<?php bloginfo('template_url' ); ?>/images/lady-in-yoga.jpg" alt="lady doing yoga" class="figure">
 		</div>
 	</div>
 	<?php 
--- single.php
+++ single.php
@@ -1,30 +1,98 @@
 <?php 
-	/**
-	 * single.php
-	 */
 	get_header(); 
 ?>
+	<div id="header">
+		<h1><a href="<?php echo get_home_url(); ?>">Belle &amp; Carrie <span>Rehabilitation Yoga</span></a></h1>
+		<ul id="navigation">
+			<li>
+				<a href="<?php echo get_home_url(); ?>">Home</a>
+			</li>
+			<li>
+				<a href="<?php echo get_home_url(); ?>/about/">About</a>
+			</li>
+			<li>
+				<a href="<?php echo get_home_url(); ?>/classes/">Classes</a>
+				<ul>
+					<li>
+						<a href="<?php echo get_home_url(); ?>/instructors/">Instructors</a>
+					</li>
+				</ul>
+			</li>
+			<li>
+				<a href="<?php echo get_home_url(); ?>/contact/">Contact</a>
+			</li>
+			<li>
+				<a href="<?php echo get_home_url(); ?>/blog/">Blog</a>
+			</li>
+		</ul>
+	
+	</div>
+	<div id="body">
+		<h2>Single Post</h2>
+		<div class="content">
+			<div class="article">
+				
+				<?php
+					if (have_posts()) {
+						while (have_posts()) {
+							the_post();
+							
+							if (is_single()) {
+								echo '<h3>'. get_the_title() .'</h3>';
+								?>
+								<span><?php echo date('F j, Y'); ?></span>
+								<?php
+								the_content(); // используем функцию вне условия
+							} else {
+								echo '<h3><a href="'. get_permalink() .'">'. get_the_title() .'</a></h3>';
+							}
+							
+						}
+					} else {
+						echo '<p>Записей нет...</p>';
+					}
+				?>
+			</div>
+			<div class="sidebar">
+				<h3>Recent Posts</h3>
+	
+				<ul>
+ 
+				<?php 
+				//
+$args = [
+	'posts_per_page'      => 3,
+	'orderby'          => 'post_date',
+	'order'            => 'DESC',
+	'include'          => '',
+	'exclude'          => '',
+	'meta_key'         => '',
+	'meta_value'       => '',
+	'post_type'        => 'post',
+	'post_status'      => 'publish',
+	'suppress_filters' => true,
+]; 
 
-<?php get_header(); ?>
-<p>test css</p>
-	<div class="middle">
-		<?php the_post(); ?>
-		
-		<h1><?php the_title() ?></h1>
-		<?php the_content(); ?>
-		<div>
-			<?php the_post_thumbnail(); ?>
-		</div>
-		<div class="author">
-			Автор поста: <?php the_author(); ?>
-		</div>
-		<div class="date">
-
-			Дата:
-			<?php
-			$d = 'd.m.Y';
-			the_time( $d );
-			?>
+				//
+				    $wp_query = new WP_Query(); 
+				    $wp_query->query($args );
+				    while ($wp_query->have_posts()) : 
+				        $wp_query->the_post(); ?>
+				        <li>
+				        	<h4>
+				        		<a href="<?php the_permalink(); ?>" title="Read more"><?php the_title(); ?></a>
+				        	</h4>
+				        	<span><?php the_time('F j, Y'); ?></span>
+				        </li>
+				 
+				        <?php endwhile; ?>
+				 
+				        <?php wp_reset_postdata(); ?>
+				 
+				    </ul>
+			</div>
 		</div>
 	</div>
-<?php get_footer(); ?>
+	<?php 
+	get_footer(); 
+	?>
